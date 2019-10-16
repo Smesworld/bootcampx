95ae2245d4b635516e29f7450452294615d1f267
Alan Dodge <alphaechododge@gmail.com>
2019-10-16 19:24:43 +0000

    Updated students and teachers to protect against sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -13,9 +13,9 @@ pool.query(`
 SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${args[0]}%'
-LIMIT ${args[1]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, [args[0], args[1]])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -15,9 +15,9 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${args[0]}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+`, [args[0]])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
